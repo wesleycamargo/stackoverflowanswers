c4224427caf8c6da4c700b383b86a10c09a6acda
Wesley Camargo <wesley_camargo@epam.com>
2022-09-26 15:19:44 +0200

    azure devops dynamic branch

--- bicep/managementGroup/Grant-SPNPermission.ps1
+++ bicep/managementGroup/Grant-SPNPermission.ps1
@@ -1,13 +1,10 @@
-#sign in to Azure from Powershell, this will redirect you to a webbrowser for authentication, if required
+
 Connect-AzAccount
+$user = Get-AzADUser -SignedIn
 
-#get object Id of the current user (that is used above)
-$user = Get-AzADUser -UserPrincipalName (Get-AzContext).Account
-
-#assign Owner role at Tenant root scope ("/") as a User Access Administrator to current user
 New-AzRoleAssignment -Scope '/' -RoleDefinitionName 'Owner' -ObjectId $user.Id
 
-#(optional) assign Owner role at Tenant root scope ("/") as a User Access Administrator to service principal (set $spndisplayname to your service principal displayname)
-$spndisplayname = "StackOverflowAnswers"
+
+$spndisplayname = "<SPN Display Name>"
 $spn = (Get-AzADServicePrincipal -DisplayName $spndisplayname).id
 New-AzRoleAssignment -Scope '/' -RoleDefinitionName 'Owner' -ObjectId $spn
